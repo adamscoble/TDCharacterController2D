8fe1e41f15785dbc4015ccb999e9b83eb3543c9d
Adam Scoble <adam.scoble@gmail.com>
2018-08-10 17:55:43 +1000

    Replaced OverlapCircle with CircleCast  to determine slide position

--- Assets/TDCharacterController2D.cs
+++ Assets/TDCharacterController2D.cs
@@ -10,23 +10,21 @@ public class TDCharacterController2D : MonoBehaviour {
 	public LayerMask CollisionLayers;
 	[Tooltip("Additional distance between the CircleCollider2D's radius and collision testing.")]
     public float RadiusBuffer;
+	[Tooltip("The minimum distance the TDCharacterController2D requires to move. Can prevent slight stutter in certain scenarios (i.e. moving against corners).")]
+	public float MinimumMovementThreshold = 0.1f;
 
 	CircleCollider2D _collider;
 	float _radius;
-	/// <summary>The minimum distance the TDCharacterController2D requires to move. Can prevent slight stutter in certain scenarios (i.e. moving against corners).</summary>
-	float _minimumMovementThreshold;
 	float _sqrMinimumMovementThreshold;
 
 	readonly RaycastHit2D[] _collisions = new RaycastHit2D[2];
-	readonly Collider2D[] _overlaps = new Collider2D[2];
 
     void Awake() {
 	    if (Transform == null) { Transform = transform; }
 	    _collider = GetComponent<CircleCollider2D>();
 
 	    _radius = _collider.radius + RadiusBuffer;
-	    _minimumMovementThreshold = _radius / 50f;
-	    _sqrMinimumMovementThreshold = _minimumMovementThreshold * _minimumMovementThreshold;
+	    _sqrMinimumMovementThreshold = MinimumMovementThreshold * MinimumMovementThreshold;
 
 	    SetRigidbodyKinematic();
 	}
@@ -61,15 +59,11 @@ public class TDCharacterController2D : MonoBehaviour {
 
 		if (IsPositionValid(collisionCount, _collisions[0].collider)) { return currentPosition + motion; }
 
-		RaycastHit2D hit = _collisions.FirstOrDefault(h => h.collider != _collider);
-
-		return GetValidPositionFromCollision(currentPosition, direction, distance, hit);
+		return GetValidPositionFromCollision(currentPosition, direction, distance, _collisions.First(h => h.collider != _collider));
 	}
 
 	Vector2 GetValidPositionFromCollision(Vector2 currentPosition, Vector2 direction, float distance, RaycastHit2D hit) {
-		Vector2 positionAtCollision = currentPosition + (direction * (distance * hit.fraction));
-		positionAtCollision += hit.normal * _minimumMovementThreshold; // Move slightly further away from the point of collision to account for inaccuracy
-
+		Vector2 positionAtCollision = GetPositionAtCollision(currentPosition, direction, distance, hit);
 		float normalizedAngle = GetNormalizedAngle(-direction, hit.normal);
 
 		if (Math.Abs(normalizedAngle) < 0.01f) { return positionAtCollision; } // We have walked straight into the collision's normal
@@ -77,25 +71,28 @@ public class TDCharacterController2D : MonoBehaviour {
 		float remainingDistance = distance * (1 - hit.fraction); // The amount of motion remaining at the point of collision
 		remainingDistance *= Mathf.Abs(normalizedAngle); // Reduce the remaining distance according to the angle of the collision (moving directly into a collider's normal would result in 0 remaining distance)
 
-		Vector2 adjustedDirection = Quaternion.AngleAxis(normalizedAngle < 0 ? -90 : 90, Vector3.back) * hit.normal; // Rotate the collision's normal by 90 degrees towards the direction of movement to adjust direction
-		adjustedDirection *= remainingDistance;
+		Vector2 slideDirection = Quaternion.AngleAxis(normalizedAngle < 0 ? -90 : 90, Vector3.back) * hit.normal; // Rotate the collision's normal by 90 degrees towards the direction of movement to slide along surface
 
-		Vector2 adjustedPosition = positionAtCollision + adjustedDirection;
+		int collisionCount = Physics2D.CircleCastNonAlloc(positionAtCollision, _radius, slideDirection, _collisions, remainingDistance, CollisionLayers);
 
-		int overlapCount = Physics2D.OverlapCircleNonAlloc(adjustedPosition, _radius, _overlaps, CollisionLayers);
+		if(IsPositionValid(collisionCount, _collisions[0].collider)) { return positionAtCollision + (slideDirection * remainingDistance); }
 
-		if(IsPositionValid(overlapCount, _overlaps[0])) { return adjustedPosition; }
+		RaycastHit2D slideHit = _collisions.First(h => h.collider != _collider);
+		float slideHitDistance = remainingDistance * slideHit.fraction;
 
-		overlapCount = Physics2D.OverlapCircleNonAlloc(positionAtCollision, _radius, _overlaps, CollisionLayers);
-
-		if (IsPositionValid(overlapCount, _overlaps[0])) { return positionAtCollision; }
-
-		return positionAtCollision + hit.normal * _minimumMovementThreshold;
+		return slideHitDistance <= MinimumMovementThreshold ? positionAtCollision : GetPositionAtCollision(positionAtCollision, slideDirection, remainingDistance, slideHit);
 	}
 
 	/// <summary>Helper function to test if a position is empty or if the only collider present is owned by this <see cref="TDCharacterController2D"/>.</summary>
 	bool IsPositionValid(int colliderCountAtPosition, Collider2D firstColliderAtPosition) {
 		return colliderCountAtPosition == 0 || colliderCountAtPosition == 1 && firstColliderAtPosition == _collider;
+	}
+
+	Vector2 GetPositionAtCollision(Vector2 position, Vector2 direction, float distance, RaycastHit2D hit) {
+		Vector2 positionAtCollision = position + (direction * (distance * hit.fraction));
+		positionAtCollision += hit.normal * MinimumMovementThreshold; // Move slightly further away from the point of collision to account for inaccuracy
+
+		return positionAtCollision;
 	}
 
 	/// <summary>Returns a signed, normalized angle between <paramref name="a"/> and <paramref name="b"/>. 0 means they are the same, while -1 and 1 mean that <paramref name="b"/> is 90 degrees right or left from <paramref name="a"/>.</summary>
