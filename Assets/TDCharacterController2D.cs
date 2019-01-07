cab7e6353b7a0851f95fd3e2e9383366868c81e6
Adam Scoble <adam.scoble@gmail.com>
2019-01-07 13:10:37 +1100

    Attempts to push itself away from collision again (broke in last commit). Additionally added an iterations value, so it can attempt the push multiple times in a single move command and happen almost instantly, instead of crawling out slowly.

--- Assets/TDCharacterController2D.cs
+++ Assets/TDCharacterController2D.cs
@@ -12,12 +12,15 @@ public class TDCharacterController2D : MonoBehaviour {
     public float RadiusBuffer;
 	[Tooltip("The minimum distance the TDCharacterController2D requires to move. Can prevent slight stutter in certain scenarios (i.e. moving against corners).")]
 	public float MinimumMovementThreshold = 0.1f;
+	[Tooltip("If the TDCharacterController2D is stuck on a collider after movement, it will attempt to push itself away from the collision up to this many times.")]
+	public int PushAwayFromCollisionIterations = 20;
 
 	CircleCollider2D _collider;
 	float _radius;
 	float _sqrMinimumMovementThreshold;
 
-	readonly RaycastHit2D[] _collisions = new RaycastHit2D[2];
+	static readonly RaycastHit2D[] _collisions = new RaycastHit2D[2];
+	static readonly Collider2D[] _overlaps = new Collider2D[2];
 
     void Awake() {
 	    if (Transform == null) { Transform = transform; }
@@ -40,7 +43,7 @@ public class TDCharacterController2D : MonoBehaviour {
 		rigidbody2D.isKinematic = true;
 	}
 
-	/// <summary>Attempts to move the controller by <paramref name="motion"/>, the motion will only be constained by collisions. It will slide along colliders.</summary>
+	/// <summary>Attempts to move the controller by <paramref name="motion"/>, the motion will only be constrained by collisions. It will slide along colliders.</summary>
 	public void Move(Vector2 motion) {
 		Vector3 currentPosition = Transform.position;
 		Vector3 newPosition = GetNewPosition(currentPosition, motion);
@@ -55,9 +58,7 @@ public class TDCharacterController2D : MonoBehaviour {
 		Vector2 direction = motion.normalized;
 		float distance = motion.magnitude;
 
-		int collisionCount = Physics2D.CircleCastNonAlloc(currentPosition, _radius, direction, _collisions, distance, CollisionLayers);
-
-		if (IsPositionValid(collisionCount, _collisions[0].collider)) { return currentPosition + motion; }
+		if (IsMovementValid(currentPosition, direction, distance)) { return currentPosition + motion; }
 
 		return GetValidPositionFromCollision(currentPosition, direction, distance, _collisions.First(h => h.collider != _collider));
 	}
@@ -73,19 +74,33 @@ public class TDCharacterController2D : MonoBehaviour {
 
 		Vector2 slideDirection = Quaternion.AngleAxis(normalizedAngle < 0 ? -90 : 90, Vector3.back) * hit.normal; // Rotate the collision's normal by 90 degrees towards the direction of movement to slide along surface
 
-		int collisionCount = Physics2D.CircleCastNonAlloc(positionAtCollision, _radius, slideDirection, _collisions, remainingDistance, CollisionLayers);
-
-		if(IsPositionValid(collisionCount, _collisions[0].collider)) { return positionAtCollision + (slideDirection * remainingDistance); }
+		if(IsMovementValid(positionAtCollision, slideDirection, remainingDistance)) { return positionAtCollision + (slideDirection * remainingDistance); }
 
 		RaycastHit2D slideHit = _collisions.First(h => h.collider != _collider);
 		float slideHitDistance = remainingDistance * slideHit.fraction;
 
-		return slideHitDistance <= MinimumMovementThreshold ? positionAtCollision : GetPositionAtCollision(positionAtCollision, slideDirection, remainingDistance, slideHit);
+		if (slideHitDistance > MinimumMovementThreshold) { return GetPositionAtCollision(positionAtCollision, slideDirection, remainingDistance, slideHit); }
+
+		int pushAwayFromCollisionIterations = PushAwayFromCollisionIterations;
+
+		while (pushAwayFromCollisionIterations > 0 && !IsPositionValid(positionAtCollision, -MinimumMovementThreshold)) {
+			positionAtCollision += hit.normal * MinimumMovementThreshold;
+
+			pushAwayFromCollisionIterations--;
+		}
+
+		return positionAtCollision;
 	}
 
-	/// <summary>Helper function to test if a position is empty or if the only collider present is owned by this <see cref="TDCharacterController2D"/>.</summary>
-	bool IsPositionValid(int colliderCountAtPosition, Collider2D firstColliderAtPosition) {
-		return colliderCountAtPosition == 0 || colliderCountAtPosition == 1 && firstColliderAtPosition == _collider;
+	/// <summary>Returns true if there are no collisions along the movement (excepting this <see cref="TDCharacterController2D"/>'s collider).<para>Collision results are stored in <see cref="_collisions"/>.</para></summary>
+	bool IsMovementValid(Vector2 position, Vector2 direction, float distance) {
+		int collisionCount = Physics2D.CircleCastNonAlloc(position, _radius, direction, _collisions, distance, CollisionLayers);
+
+		return IsNullOrSelfCollision(collisionCount, _collisions[0].collider);
+	}
+
+	bool IsNullOrSelfCollision(int colliderCount, Collider2D firstCollider) {
+		return colliderCount == 0 || (colliderCount == 1 && firstCollider == _collider);
 	}
 
 	Vector2 GetPositionAtCollision(Vector2 position, Vector2 direction, float distance, RaycastHit2D hit) {
@@ -98,5 +113,12 @@ public class TDCharacterController2D : MonoBehaviour {
 	/// <summary>Returns a signed, normalized angle between <paramref name="a"/> and <paramref name="b"/>. 0 means they are the same, while -1 and 1 mean that <paramref name="b"/> is 90 degrees right or left from <paramref name="a"/>.</summary>
 	static float GetNormalizedAngle(Vector2 a, Vector2 b) {
 		return -a.x * b.y + a.y * b.x;
+	}
+
+	/// <summary>Returns true if there are no overlaps at <paramref name="position"/> (excepting this <see cref="TDCharacterController2D"/>'s collider).<para>Overlap results are stored in <see cref="_overlaps"/>.</para></summary>
+	bool IsPositionValid(Vector2 position, float radiusAdjustment = 0) {
+		int overlapCount = Physics2D.OverlapCircleNonAlloc(position, _radius + radiusAdjustment, _overlaps, CollisionLayers);
+
+		return IsNullOrSelfCollision(overlapCount, _overlaps[0]);
 	}
 }
